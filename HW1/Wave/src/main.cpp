b9fa7da76fe6f7e9cc7690862db444a7c4cd6865
Nhan <hieu-nhan.tran@polymtl.ca>
2025-08-27 00:39:45 -0400

    folder setup

---
+++ HW1/Wave/src/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <cmath>
+#include <omp.h>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+std::tuple<int, double, double> readInputFile(const std::string& filename) {
+    std::ifstream input_file(filename);
+    if (!input_file) {
+        std::cerr << "Error opening input file: " << filename << std::endl;
+        return {};
+    }
+
+    std::string line;
+    std::vector<std::string> values;
+    while (std::getline(input_file, line)) {
+        std::istringstream iss(line);
+        std::string key;
+        char equalSign;
+        if (iss >> key >> equalSign && equalSign == '=') {
+            std::string value;
+            if (iss >> value) {
+                std::cout << "Key: " << key << ", Value: " << value << std::endl;
+                values.push_back(value);
+            }
+        }
+    }
+    int N = std::stoi(values[0]);
+    double c = std::stod(values[1]);
+    double CFL = std::stod(values[2]);
+    return {N, c, CFL};
+}
+
+int main(int argc, char* argv[]) {
+    // Ensure the input file is passed as a command line argument
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_filename>" << std::endl;
+        return 1;
+    }
+
+    std::string input_filename = argv[1]; // The input filename is passed as the first argument
+
+    std::cout << "Input file: " << input_filename << std::endl;
+
+    auto [N, c, CFL] = readInputFile(input_filename);
+
+    return 0;
+}
