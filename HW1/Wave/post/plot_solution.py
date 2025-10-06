a09b47b583e7e8c929834120e1308bef12fa2952
jatred <jacob.tremblay188@gmail.com>
2025-10-06 15:40:40 -0400

    post process

--- HW1/Wave/post/plot_solution.py
+++ HW1/Wave/post/plot_solution.py
@@ -6,6 +6,7 @@ import imageio
 
 
 output_filename = '../output_CFL1.txt'
+method = "Hybride_Theta=1.0"
 data = np.loadtxt(output_filename)
 
 x = data[0, 1:]
@@ -32,29 +33,28 @@ for i in range(len(t)):
     plt.close("all")
 
 gif_filename = output_filename.split('.')[-2].split("/")[-1] + '.gif'
-imageio.mimsave(gif_filename, frames, fps=20)
-print(f'GIF saved as {gif_filename}')
+imageio.mimsave('output/' + method.lower() + '.gif', frames, fps=20)
+# imageio.mimsave('output/' + gif_filename, frames, fps=20)
+print(f'GIF saved as {method.lower()}.gif')
 
 
 
 
+output_filename_obs = '../output_CFL1_obs.txt'
+data_obs = np.loadtxt(output_filename_obs)
 
-# output_filename_obs = '../output_CFL1_obs.txt'
-# data_obs = np.loadtxt(output_filename_obs)
+u_obs = data_obs[1, 1:]
+t_obs = data_obs[1, 0]
 
-# u_obs = data_obs[1, 1:]
-# t_obs = data_obs[1, 0]
-
-# fig,ax = plt.subplots(figsize=(8,6))
-# ax.plot(x, u_obs)
-# ax.set_title(f'Observations pour une mi-onde théorique à x=2.5 \n Temps = {t_obs:.2f} secondes')
-# ax.set_xlabel('Position (x)')
-# ax.set_ylabel('Amplitude (u)')
-# ax.yaxis.set_label_coords(-0.1, 0.5)
-# ax.set_xlim(np.min(x), np.max(x))
-# ax.set_ylim(min(1.1*np.min(u[i]),0), max(1.1*np.max(u[i]),0))
-# ax.grid()
-# plt.show()
-
-
+fig,ax = plt.subplots(figsize=(5.5,4))
+ax.plot(x, u_obs)
+ax.set_title(f'{method.replace('_',' ')} \n Temps = {t_obs:.2f} secondes')
+ax.set_xlabel('Position (x)')
+ax.set_ylabel('Amplitude (u)')
+ax.yaxis.set_label_coords(-0.1, 0.5)
+ax.set_xlim(np.min(x), np.max(x))
+ax.set_ylim(min(1.1*np.min(u_obs),0), max(1.1*np.max(u_obs),0))
+ax.grid()
+plt.savefig('output/' + method.lower() + '.pdf')
+plt.show()
 
